d7b7af90764f0adfa18d2443b2f8e4dbad68981c
ABDUL RAFAY <lifewithabdulrafay@gmail.com>
2024-11-11 23:05:21 +0500

    Mcq 1 Solution

--- mcq1.cpp
+++ mcq1.cpp
@@ -45,3 +45,4 @@ program outputs: -1, 0
 */
 
 //Answer: program outputs: 0, -1
+//Explanation: v2.reserve(10) only reserves memory for 10 elements, but the size of v2 is still 0. v2.push_back(i) adds elements to the vector, so v2[0] is 0. v1[0] is -1 because v1 was initialized with 10 elements, all set to -1.
