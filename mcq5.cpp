b83a871c67b4c4ebd4dd2bda27256a2acbf31f93
ABDUL RAFAY <lifewithabdulrafay@gmail.com>
2024-11-11 23:36:43 +0500

    Mcq 5 Solution

---
+++ mcq5.cpp
@@ -0,0 +1,53 @@
+//What will happen when you attempt to compile and run the following code?
+#include <iostream>
+
+#include <algorithm>
+
+#include <vector>
+
+#include <deque>
+
+#include <set>
+
+using namespace std;
+
+int
+main ()
+{
+
+  int mynumbers[] = { 8, 9, 7, 6, 4, 1 };
+
+  vector < int >v1 (mynumbers, mynumbers + 6);
+
+  deque < int >d1 (mynumbers, mynumbers + 6);
+
+  set < int >s1 (mynumbers, mynumbers + 6);
+
+  vector < int >::iterator found = find (v1.begin (), v1.end (), 3);
+
+  if (found != v1.end ())
+
+    cout << "found" << ", ";
+
+  cout << find (d1.begin (), d1.end (), 9) << ", ";    //LINE I
+
+  cout << find (s1.begin (), s1.end (), 6);    //LINE II
+
+  return 0;
+
+}
+
+//Options:
+
+/*
+program output: found, 8, 9, 7, 6, 4, 1, 1, 4, 6, 7, 8, 9,
+program output: 9, 6,
+compilation error in LINE II
+runtime error at LINE I
+program output: 8, 9, 7, 6, 4, 1, 1, 4, 6, 7, 8, 9,
+compilation error in LINE I
+*/
+
+//Answer: compilation error in LINE I
+//Explanation: The program outputs: compilation error in LINE I. The find function returns an iterator, so you should dereference the iterator to get the value. The program outputs: found, 8, 9, 7, 6, 4, 1, 1, 4, 6, 7, 8, 9, because the find function returns an iterator to the first element found in the range. The program doesn't output 9, 6 because the find function returns an iterator to the first element found in the range. There are no runtime errors in the code.
+
