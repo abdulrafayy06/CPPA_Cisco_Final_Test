63b46dbb814c79a86281d25b995bff492f777ce1
ABDUL RAFAY <lifewithabdulrafay@gmail.com>
2024-11-13 19:13:32 +0500

    Mcq 24 Solution

---
+++ mcq24.cpp
@@ -0,0 +1,14 @@
+/*
+Which methods from std::deque class can be used to check if there are elements in the container. Choose all that apply:
+
+// Options:
+isempty()
+isfull()
+there is no such method
+empty()
+size()
+clear()
+*/
+
+// Answer: empty(), size()
+// Explanation: The std::deque class provides the following methods to check if there are elements in the container:
